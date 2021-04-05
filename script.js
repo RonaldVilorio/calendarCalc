ce5ea222e12d55c70d383238f80555a147a07d08
RonaldVilorio <ronvilorio@gmail.com>
2021-04-05 13:07:19 -0400

    calc func attached to html, starter css added

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("hey")
+const submitButton = document.querySelector("#submitButton");
+submitButton.addEventListener("click", calculate);
+
+function calculate(e) {
+  e.preventDefault();
+  const date = document.querySelector("#Date").value;
+
+  console.log(date);
+}

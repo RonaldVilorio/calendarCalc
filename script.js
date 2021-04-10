88918cdc52fc02247797b412a1cf395eca81c6e9
RonaldVilorio <ronvilorio@gmail.com>
2021-04-10 18:02:58 -0400

    extra Days obj added,logic to find days/year commented

--- script.js
+++ script.js
@@ -10,6 +10,24 @@ function calculate(e) {
   } else {
     // no leap
   }
+let extraDaysPerMonth = {
+  1: 3,
+  2: 3,
+  3: 6,
+  4: 8,
+  5: 11,
+  6: 13,
+  7: 16,
+  8: 19,
+  9: 22,
+  10: 24,
+  11: 26,
+  12: 29,
+}
+// birthMonth - 1 * 28 + day + extraDatPerMonth - 1 = days
+// ex: 11/22
+//  (10) * 28 + 22 + (10) = 326 days/year
+  
   // obj with the old calendar
   // obj with new calendar
   // match the days from old into the new calendar
